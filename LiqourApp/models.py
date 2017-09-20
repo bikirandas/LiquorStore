55118dc095c5da28037b706ade3f4d1bbdd5bb08
Rony <bd15@iitbbs.ac.in>
2017-09-20 14:34:55 +0530

    PY-2017.1.2 <Rony@Rony-PC Overwrite remote D:\Rony Important\Start-up\Python\StartupApp\LiquorStore to local

--- LiqourApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class City(models.Model):
-    city_name = models.CharField(max_length=30)
-    city_pin_start = models.IntegerField()
-    city_pin_end = models.IntegerField()
