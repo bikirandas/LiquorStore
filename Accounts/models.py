b0073a6235eed550ab6f4b09eb17c90abd0cd0f7
Rony <bd15@iitbbs.ac.in>
2017-09-24 05:11:03 +0530

    Commit to github - Changes in registration models

--- Accounts/models.py
+++ Accounts/models.py
@@ -10,6 +10,7 @@ class UserProfile(models.Model):
     email = models.EmailField(unique=True, max_length=50)
     first_name = models.CharField(max_length=20, blank=True)
     last_name = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
     profile_pic = models.ImageField(upload_to='Accounts/Profile', blank=True)
 
     def __str__(self):
