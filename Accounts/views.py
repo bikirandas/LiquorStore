1e3acc4255141a4f8a608ae0802e658e8fc0ef72
Rony <bd15@iitbbs.ac.in>
2017-09-26 01:05:33 +0530

    Commit to github - Changes in registration models

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm, UserProfileForm
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from LiqourApp.views import read_file
 from LiquorStore.settings import BASE_DIR
@@ -8,10 +9,13 @@ import os
 from .models import UserProfile
 
 
+fp = os.path.join(BASE_DIR, 'LiqourApp\static\\appdata\menubar.txt')
+head_list = read_file(fp)
+
+
 def register(request):
     registered = False
-    fp = os.path.join(BASE_DIR, 'LiqourApp\static\\appdata\menubar.txt')
-    head_list = read_file(fp)
+
     if request.method == 'POST':
         reg_form = RegistrationForm(data=request.POST)
         profile_form = UserProfileForm(data=request.POST)
@@ -41,4 +45,35 @@ def register(request):
         profile_form = UserProfileForm()
         args = {'reg_form': reg_form, 'profile_form': profile_form}
         head_list.update(args)
+        print(head_list)
         return render(request, 'registration.html', head_list)
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Django's built-in authentication function:
+        user = authenticate(username=username, password=password)
+
+        # If we have a user
+        if user:
+            # Check it the account is active
+            if user.is_active:
+                # Log the user in.
+                login(request, user)
+                # Send the user back to some page.
+                # In this case their homepage.
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                # If account is not active:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        # Nothing has been provided for username or password.
+        return render(request, 'login.html', head_list)
